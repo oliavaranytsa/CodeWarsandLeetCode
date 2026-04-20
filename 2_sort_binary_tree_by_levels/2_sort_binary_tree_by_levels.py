195e00e31d8d85e741f938b6acaa19138a01d00a
Olga Varanytsia <varanytsa.pn@ucu.edu.ua>
2026-04-20 17:07:20 +0300

    minor fixes, but test 2 is failed

--- 2_sort_binary_tree_by_levels/2_sort_binary_tree_by_levels.py
+++ 2_sort_binary_tree_by_levels/2_sort_binary_tree_by_levels.py
@@ -1,18 +1,18 @@
 def tree_by_levels(node):
     if node is None:
-        return False
+        return []
 
     result = []
     queue = [node]
 
-    while queue is not None:
+    while queue:
         current = queue.pop(0)
-        result.append(current)
+        result.append(current.value)
 
         if current.left is not None:
-            result.append(current)
+            result.append(current.left)
 
         if current.right is not None:
-            result.append(current)
+            result.append(current.right)
 
     return result
